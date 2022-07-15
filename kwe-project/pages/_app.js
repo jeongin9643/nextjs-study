6141632247c08a72e87ba6d92789f6876d5db5f3
jeongin9643 <jeongin9643@gmail.com>
2022-07-15 22:07:17 +0900

    add navbar

--- kwe-project/pages/_app.js
+++ kwe-project/pages/_app.js
@@ -1,19 +1,88 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(()=>{
+  useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
-},[])
+  }, []);
 
   return (
     <>
-      <div>
-        navbar
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Kwe
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link href="/">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/about">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    About
+                  </a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/product">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Product
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container-fluid">
+        <div className="row">
+          <nav
+            id="sidebarMenu"
+            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+          >
+            <div className="position-sticky pt-3">
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    <span
+                      data-feather="home"
+                      className="align-text-bottom"
+                    ></span>
+                    Dashboard
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+              <Component {...pageProps} />
+            </div>
+          </main>
+        </div>
       </div>
-      < Component {...pageProps} />
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
